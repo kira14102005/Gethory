ebb5610aa52543c74f12b131a49f0a39d86d1551
Harshit Rai <raiharshit66@gmail.com>
2025-10-19 22:14:06 +0530

    Minr fix regarding cors origin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -28,7 +28,7 @@ export const io = new Server(server, {
 })
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: APP_ORIGIN,
     credentials: true
 }))
 app.use(express.json({ limit: '10mb' }));
