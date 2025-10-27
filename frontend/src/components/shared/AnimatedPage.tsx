d8b0959bd142d58f5219a73e3360cf660be2798d
Harshit Rai <raiharshit66@gmail.com>
2025-10-27 22:54:22 +0530

    Change the build with framer motion and also the dependencies

--- frontend/src/components/shared/AnimatedPage.tsx
+++ frontend/src/components/shared/AnimatedPage.tsx
@@ -1,27 +1,43 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+// src/components/shared/AnimatedPage.tsx
+import React from "react";
+import { motion } from "framer-motion";
 
 const pageVariants = {
-  initial: { opacity: 0, x: -50, scale: 0.98 },
-  in: { opacity: 1, x: 0, scale: 1 },
-  out: { opacity: 0, x: 50, scale: 1.02 }
+  initial: {
+    opacity: 0,
+    y  : -100,
+    scale: 0.98,
+    rotate: 0
+  },
+  in: {
+    opacity: 1,
+    y  : 0,
+    scale: 1,
+    rotate: 0
+  },
+  out: {
+    opacity: 0,
+    y  : 100,
+    scale: 1.01,
+    rotate: 0
+  }
 };
 
 const pageTransition = {
-  type: 'tween',
-  ease: 'anticipate',
-  duration: 0.5
+  type: "spring",
+  stiffness: 70,
+  damping: 20
 };
 
-const AnimatedPage = ({ children  } :  {children  : React.ReactNode}) => (
+const AnimatedPage = ({ children } :  {children : React.ReactNode}) => (
   <motion.div
+    variants={pageVariants}
     initial="initial"
     animate="in"
     exit="out"
-    variants={pageVariants}
     //@ts-ignore
     transition={pageTransition}
-    style={{ height: '100%' }} // optional, adjust layout
+    style={{ minHeight: "100vh" }}
   >
     {children}
   </motion.div>
