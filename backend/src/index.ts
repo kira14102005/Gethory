befdc53a88c56f072f45897732e0b12542733cab
Harshit Rai <raiharshit66@gmail.com>
2026-09-19 19:42:15 +0530

    add:Add cookie-parser middleware before all middlewares

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -31,6 +31,7 @@ app.use(cors({
     origin: APP_ORIGIN,
     credentials: true
 }))
+app.use(cookieParser());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use("/api/storage",
